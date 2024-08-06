480e6f8a244dc1d7f2cc191f54972b71f1f12164
AlpHa47DeV <deitatwo706@gmail.com>
2024-08-06 07:37:21 +0530

    Update scripts.js

--- src/script/scripts.js
+++ src/script/scripts.js
@@ -2,18 +2,31 @@ function loadVideo() {
     const fileInput = document.getElementById('fileInput');
     const urlInput = document.getElementById('urlInput');
     const videoPlayer = document.getElementById('videoPlayer');
-    const videoSource = document.getElementById('videoSource');
 
     if (fileInput.files.length > 0) {
         const file = fileInput.files[0];
-        videoSource.src = URL.createObjectURL(file);
+        const fileURL = URL.createObjectURL(file);
+        setVideoSource(fileURL);
         videoPlayer.load();
         videoPlayer.play();
+        handleFile(file);
     } else if (urlInput.value.trim() !== "") {
-        videoSource.src = urlInput.value.trim();
+        const url = urlHandler(urlInput.value.trim());
+        setVideoSource(url);
         videoPlayer.load();
         videoPlayer.play();
+        handleUrl(url);
     } else {
         alert("Please select a file or enter a URL.");
     }
+}
+
+function setVideoSource(src) {
+    const mp4Source = document.getElementById('mp4Source');
+    const webmSource = document.getElementById('webmSource');
+    const mkvSource = document.getElementById('mkvSource');
+
+    mp4Source.src = src + ".mp4";
+    webmSource.src = src + ".webm";
+    mkvSource.src = src + ".mkv";
 }
