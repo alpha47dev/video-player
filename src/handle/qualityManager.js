cdf122fdf55a4d193dea4d6af04e724af8c342bd
AlpHa47DeV <deitatwo706@gmail.com>
2024-08-06 07:39:42 +0530

    Create qualityManager.js

---
+++ src/handle/qualityManager.js
@@ -0,0 +1,9 @@
+function handleFile(file) {
+    // Implement quality management logic here
+    console.log("Handling file:", file);
+}
+
+function handleUrl(url) {
+    // Implement quality management logic here
+    console.log("Handling URL:", url);
+}
